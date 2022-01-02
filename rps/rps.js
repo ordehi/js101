399b824d5a80a2b1904eb681fcb0ca9527815d29
Joshua Ordehi <mev.dev.kar@gmail.com>
2022-01-02 17:31:11 -0400

    remove comments from rps

--- rps/rps.js
+++ rps/rps.js
@@ -1,27 +1,3 @@
-/* START
-
-SET playerChoice = null
-SET computerChoice = null
-
-WHILE true
-
-- GET playerChoice = getUserInput('Enter your playerChoice. Rock: r or 1, Paper: p or 2, Scissors: s or 3')
-  - IF playerChoice is not 1-3 or r, p, or s
-    - GET playerChoice = getUserInput('Incorrect playerChoice, you entered playerChoice, but you must choose r or 1 for Rock, p or 2 for Paper, s or 3 for Scissors)
-  - IF playerChoice is r, p, or s playerChoice = 1, 2 or 3
-- GET computerChoice = Math.floor(Math.random() \* 3) + 1
-- IF playerChoice == computerChoice
-  - PRINT draw, choose again
-- IF playerChoice > computerChoice
-  - PRINT player wins
-- ELSE
-  - PRINT computer wins
-- GET goAGain = getUserInput('want to play again?')
-- IF goAGain != yes
-  - BREAK
-
-END */
-
 const readline = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors'];
 const getUserInput = (message) => readline.question('>> ' + message + '\n>> ');
