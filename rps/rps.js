65aca978df99420efc8ea2411bbba86431019897
Joshua Ordehi <mev.dev.kar@gmail.com>
2022-01-10 21:17:20 -0400

    add bonus features to rps

--- rps/rps.js
+++ rps/rps.js
@@ -1,31 +1,55 @@
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+const INDEXED_CHOICES = ['r', 'p', 's', 'l', 'v'];
+const VALID_CHOICES = {
+  r: 'rock',
+  p: 'paper',
+  s: 'scissors',
+  l: 'lizard',
+  v: 'spock',
+};
+const WINNING_COMBOS = {
+  rock: ['scissors', 'lizard'],
+  paper: ['rock', 'spock'],
+  scissors: ['paper', 'lizard'],
+  lizard: ['paper', 'spock'],
+  spock: ['rock', 'scissors'],
+};
+
+const SCORE = {
+  player: 0,
+  computer: 0,
+  tie: 0,
+};
+
 const getUserInput = (message) => readline.question('>> ' + message + '\n>> ');
 const display = (message) => console.log(message);
 
 const getYesOrNoTo = (message) => {
   let yesOrNo = getUserInput(message).toLowerCase();
   while (yesOrNo !== 'y' && yesOrNo !== 'n') {
-    yesOrNo = getUserInput('Would you like to play again?').toLowerCase();
+    yesOrNo = getUserInput(message).toLowerCase();
   }
 
   return yesOrNo === 'y';
 };
 
-const displayWinner = (playerChoice, computerChoice) => {
-  display(`You chose ${playerChoice}, computer chose ${computerChoice}`);
+const playerWins = (playerChoice, computerChoice) =>
+  WINNING_COMBOS[VALID_CHOICES[playerChoice]].includes(computerChoice);
 
-  if (
-    (playerChoice === 'rock' && computerChoice === 'scissors') ||
-    (playerChoice === 'paper' && computerChoice === 'rock') ||
-    (playerChoice === 'scissors' && computerChoice === 'paper')
-  ) {
+const getWinner = (playerChoice, computerChoice) => {
+  if (playerWins(playerChoice, computerChoice)) {
+    return 'player';
+  } else if (playerChoice === computerChoice) {
+    return 'tie';
+  } else {
+    return 'computer';
+  }
+};
+
+const displayWinner = (winner) => {
+  if (winner === 'player') {
     display('===============\nYou win!\n===============');
-  } else if (
-    (playerChoice === 'rock' && computerChoice === 'paper') ||
-    (playerChoice === 'paper' && computerChoice === 'scissors') ||
-    (playerChoice === 'scissors' && computerChoice === 'rock')
-  ) {
+  } else if (winner === 'computer') {
     display('===============\nComputer wins!\n===============');
   } else {
     display("===============\nIt's a tie\n===============");
@@ -33,18 +57,32 @@ const displayWinner = (playerChoice, computerChoice) => {
 };
 
 while (true) {
-  let playerChoice = getUserInput(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  let formattedChoices = JSON.stringify(VALID_CHOICES)
+    .replace(/{|}|"/g, '')
+    .replace(/:/g, ' for ')
+    .replace(/,/g, ', ');
+  let playerChoice = getUserInput(`Choose one: ${formattedChoices}`);
 
-  while (!VALID_CHOICES.includes(playerChoice)) {
+  while (VALID_CHOICES[playerChoice] === undefined) {
     playerChoice = getUserInput(`${playerChoice} is not a valid playerChoice`);
   }
 
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
+  let randomIndex = Math.floor(Math.random() * INDEXED_CHOICES.length);
+  let computerChoice = VALID_CHOICES[INDEXED_CHOICES[randomIndex]];
 
-  display(`You chose ${playerChoice}, computer chose ${computerChoice}`);
+  let winner = getWinner(playerChoice, computerChoice);
+  SCORE[winner] += 1;
 
-  displayWinner(playerChoice, computerChoice);
+  display(
+    `You chose ${VALID_CHOICES[playerChoice]}, computer chose ${computerChoice}`
+  );
+
+  if (SCORE[winner] === 5) {
+    display(`${winner} wins with ${SCORE[winner]} points`);
+    break;
+  }
+
+  displayWinner(winner);
 
   if (!getYesOrNoTo('Would you like to play again? y/n')) break;
 }
