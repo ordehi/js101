84b93fd44669879da710e3b844c9d6bd54e9accc
ordehi <mev.dev.kar@gmail.com>
2022-02-23 00:57:09 -0400

    finish substrings fn

---
+++ lesson_4/substrings.js
@@ -0,0 +1,11 @@
+function substrings(str, sublength) {
+  let result = [];
+
+  for (let start = 0; start < str.length - 1; start += 1) {
+    for (let end = start + sublength; end <= str.length; end += 1) {
+      result.push(str.substring(start, end));
+    }
+  }
+
+  return result;
+}
