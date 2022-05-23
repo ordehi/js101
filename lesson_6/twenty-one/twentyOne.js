9238659c7478ce9f2dc3e7660c464364e2266f3a
Joshua Ordehi <mev.dev.kar@gmail.com>
2022-05-23 13:45:52 -0400

    complete twentyone bonus features

--- lesson_6/twenty-one/twentyOne.js
+++ lesson_6/twenty-one/twentyOne.js
@@ -2,8 +2,23 @@ const rlSync = require('../../node_modules/readline-sync');
 
 const SUITS = ['H', 'S', 'D', 'C'];
 const VALUES = [
-  '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'J',
+  'Q',
+  'K',
+  'A',
 ];
+
+const WINNING_TOTAL = 21;
+const DEALER_HIT_LIMIT = 17;
 
 function print(message) {
   return console.log('\n' + message);
@@ -19,7 +34,8 @@ function createCards() {
     VALUES.forEach((value) => cards.push([suit, value]));
   });
 
-  return shuffle(cards);
+  shuffle(cards);
+  return cards;
 }
 
 function dealCard(deck) {
@@ -43,7 +59,7 @@ function total(cards) {
   values
     .filter((value) => value === 'A')
     .forEach((_) => {
-      if (sum > 21) sum -= 10;
+      if (sum > WINNING_TOTAL) sum -= 10;
     });
 
   return sum;
@@ -56,24 +72,23 @@ function shuffle(array) {
   }
 }
 
-function busted(cards) {
-  let points = total(cards);
-  if (points > 21) return true;
-  return false;
+function busted(points) {
+  return points > WINNING_TOTAL;
 }
 
-function calculateWinner(player, dealer) {
+function calculateWinner(playerTotal, dealerTotal) {
   let results = {};
-  results.player = total(player);
-  results.dealer = total(dealer);
+  results.player = playerTotal;
+  results.dealer = dealerTotal;
   results.winner = 'Dealer'; // The house always wins
   results.loser = 'Player';
 
-  if (results.player > results.dealer) {
+  if (playerTotal > dealerTotal) {
     [results.winner, results.loser] = [results.loser, results.winner];
-  } else if (results.player === results.dealer) {
+  } else if (playerTotal === dealerTotal) {
     results.winner = 'tie';
   }
+
   return results;
 }
 
@@ -81,7 +96,11 @@ function displayWinner(results) {
   if (results.winner === 'tie') {
     print(`It's a tie!`);
   } else {
-    print(`${results.winner} wins with ${results[results.winner.toLowerCase()]} against ${results.loser}'s ${results[results.loser.toLowerCase()]}!`);
+    print(
+      `${results.winner} wins with ${
+        results[results.winner.toLowerCase()]
+      } against ${results.loser}'s ${results[results.loser.toLowerCase()]}!`
+    );
   }
 }
 
@@ -104,7 +123,7 @@ const CARD_NAMES = {
 };
 
 function prettifyCards(cards) {
-  let prettyCards = cards.map(card => {
+  let prettyCards = cards.map((card) => {
     return `${CARD_NAMES[card[1]] || card[1]} of ${CARD_NAMES[card[0]]}`;
   });
   return prettyCards;
@@ -112,86 +131,155 @@ function prettifyCards(cards) {
 
 function playerTurn(playerHand, deck, dealerHand) {
   while (true) {
-    print('Hit or stay?');
-    let answer = prompt();
-    if (answer === 'stay') break;
+    let answer = hitOrStay();
+    if (['s', 'stay'].includes(answer)) break;
     let card = dealCard(deck);
     playerHand.push(card);
+    playerHand.shift();
+    playerHand.unshift(total(playerHand));
     print(`You hit for ${prettifyCards([card])}`);
-    if (busted(playerHand)) break;
+    if (busted(playerHand[0])) break;
     announceCards(playerHand, dealerHand);
   }
 }
 
 function dealerTurn(dealerHand, deck, playerHand) {
   while (true) {
-    if (total(dealerHand) >= 17 || busted(dealerHand)) break;
+    let dealerTotal = dealerHand[0];
+    if (dealerTotal >= DEALER_HIT_LIMIT || busted(dealerTotal)) break;
     let card = dealCard(deck);
     dealerHand.push(card);
+    dealerHand.shift();
+    dealerHand.unshift(total(dealerHand));
     print(`Dealer hits for ${prettifyCards([card])}`);
     announceCards(playerHand, dealerHand);
   }
 }
 
-function announceCards(player, dealer) {
-  print(`Player cards: ${total(player)}\n${prettifyCards(player).join(' - ')}`);
-  print(`Dealer cards: ${total(dealer.slice(1))}+\n? of ? - ${prettifyCards(dealer.slice(1)).join(' - ')}`);
+function announceCards(playerHand, dealerHand) {
+  print(
+    `Player cards: ${playerHand[0]}\n${prettifyCards(playerHand.slice(1)).join(
+      ' - '
+    )}`
+  );
+  print(
+    `Dealer cards: ${total(dealerHand.slice(2))}+\n? of ? - ${prettifyCards(
+      dealerHand.slice(2)
+    ).join(' - ')}`
+  );
 }
 
-function shouldContinue() {
+function shouldContinue(gameOrMatch) {
   let answer;
   while (true) {
-    print('Do you want to play again? [Y/n]');
+    print(`Do you want to play another ${gameOrMatch}? [Y/n]`);
     answer = prompt();
     if (['y', 'n', 'yes', 'no'].includes(answer.toLowerCase())) break;
     print('Not a valid choice.');
   }
-  return answer;
+  return answer.toLowerCase();
+}
+
+function hitOrStay() {
+  let answer;
+  while (true) {
+    print('Hit or stay?');
+    answer = prompt();
+    if (['s', 'stay', 'h', 'hit'].includes(answer.toLowerCase())) break;
+    print('Not a valid choice.');
+  }
+  return answer.toLowerCase();
+}
+
+function getMatchPointMessage(scoreCard) {
+  return `Player: ${scoreCard.player}\nDealer: ${scoreCard.dealer}`;
+}
+
+function updateScoreCard(scoreCard, winner) {
+  scoreCard[winner.toLowerCase()] += 1;
+}
+
+function someoneWonMatch(scoreCard) {
+  if (scoreCard.player >= 5) return 'Player';
+  if (scoreCard.dealer >= 5) return 'Dealer';
+  return false;
+}
+
+function displayMatchResults(scoreCard, matchWinner) {
+  print(
+    `${
+      matchWinner
+        ? matchWinner + ' won the match!'
+        : 'Match ended early, no winner.'
+    } Final score is:\n${getMatchPointMessage(scoreCard)}`
+  );
 }
 
 while (true) {
-  // Initialize deck
-  let deck = createCards();
+  let scoreCard = {
+    player: 0,
+    dealer: 0,
+  };
 
-  // Deal cards to player and dealer
-  let dealerHand = [];
-  let playerHand = [];
-  dealInitialHands(playerHand, dealerHand, deck);
-
-  announceCards(playerHand, dealerHand);
+  let matchWinner = false;
 
   while (true) {
-    // Player Turn - hit or stay
-    playerTurn(playerHand, deck, dealerHand);
+    // Initialize deck
+    let deck = createCards();
 
-    // If player busts, dealer wins
-    if (busted(playerHand)) {
-      // probably end the game? or ask the user to play again?
-      print(`You busted with ${total(playerHand)}. Dealer wins.`);
-      // End loop
+    // Deal cards to player and dealer
+    let dealerHand = [];
+    let playerHand = [];
+    dealInitialHands(playerHand, dealerHand, deck);
+    dealerHand.unshift(total(dealerHand));
+    playerHand.unshift(total(playerHand));
+
+    announceCards(playerHand, dealerHand);
+
+    while (true) {
+      // Player Turn - hit or stay
+      playerTurn(playerHand, deck, dealerHand);
+
+      // If player busts, dealer wins
+      if (busted(playerHand[0])) {
+        // probably end the game? or ask the user to play again?
+        print(`You busted with ${playerHand[0]}. Dealer wins.`);
+        updateScoreCard(scoreCard, 'dealer');
+        // End loop
+        break;
+      } else {
+        print('You chose to stay!'); // if player didn't bust, must have stayed to get here
+      }
+
+      // Dealer turn, hit or stay
+      dealerTurn(dealerHand, deck, playerHand);
+
+      // If dealer busts, player wins
+      if (busted(dealerHand[0])) {
+        // probably end the game? or ask the user to play again?
+        print(`Dealer busted with ${dealerHand[0]}. Player wins!`);
+        updateScoreCard(scoreCard, 'player');
+        // End loop
+        break;
+      } else {
+        print('Dealer chose to stay!'); // if player didn't bust, must have stayed to get here
+      }
+
+      // Compare cards and declare winner
+      let gameResults = calculateWinner(playerHand[0], dealerHand[0]);
+      updateScoreCard(scoreCard, gameResults.winner);
+      displayWinner(gameResults);
       break;
-    } else {
-      print('You chose to stay!'); // if player didn't bust, must have stayed to get here
     }
 
-    // Dealer turn, hit or stay
-    dealerTurn(dealerHand, deck, playerHand);
-
-    // If dealer busts, player wins
-    if (busted(dealerHand)) {
-      // probably end the game? or ask the user to play again?
-      print(`Dealer busted with ${total(dealerHand)}. Player wins!`);
-      // End loop
-      break;
-    } else {
-      print('Dealer chose to stay!'); // if player didn't bust, must have stayed to get here
-    }
-
-    // Compare cards and declare winner
-    displayWinner(calculateWinner(playerHand, dealerHand));
-    break;
+    matchWinner = someoneWonMatch(scoreCard);
+    if (matchWinner !== false) break;
+    print(`Current match score:\n${getMatchPointMessage(scoreCard)}`);
+    let playAnotherGame = shouldContinue('game in the current match');
+    if (['n', 'no'].includes(playAnotherGame)) break;
   }
 
-  let continuePlaying = shouldContinue();
-  if (['n', 'no'].includes(continuePlaying)) break;
+  displayMatchResults(scoreCard, matchWinner);
+  let playAnotherMatch = shouldContinue('match');
+  if (['n', 'no'].includes(playAnotherMatch)) break;
 }
